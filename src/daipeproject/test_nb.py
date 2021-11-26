8664bed8a2a7534f6425f1a955b257cea836cbdc
Stanislav Daniš <90611075+Stano-D@users.noreply.github.com>
2021-11-26 15:57:43 +0100

    Test (#1)
    * test
    
    * add

---
+++ src/daipeproject/test_nb.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %run ./app/bootstrap
+
+# COMMAND ----------
+
+# MAGIC %load_ext autoreload
+# MAGIC %autoreload 2
+
+# COMMAND ----------
+
+from daipeproject.test_modelu import print_hello
+
+print_hello()
+
+# COMMAND ----------
+
+print_hello()
